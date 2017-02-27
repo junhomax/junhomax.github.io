c57bc2cb624972f26d6afcca0d236155dc0877a2
Junho Cha <ckwnsgh1991@gmail.com>
2017-02-27 22:41:42 +0900

    mod gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -52,8 +52,11 @@ gulp.task('minify-js', function() {
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+    //gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+    //    .pipe(gulp.dest('vendor/bootstrap'))
+    gupl.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/boostrap-theme.*', '!**/*.css'])
+	  .pipe(gulp.dest('vendor/bootstrap'))
+
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('vendor/jquery'))
